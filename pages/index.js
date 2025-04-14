56d1a68cb08a52aaa8873c62ee10d02078fd48be
amir <a.dooki.h@gmail.com>
2025-04-14 23:56:30 +0330

    amlak new update

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+
+import Features from "@/Components/template/index/Features";
+import Gallery from "@/Components/template/index/Gallery";
+import Homes from "@/Components/template/index/Homes";
+import Story from "@/Components/template/index/Story";
+
+import React from "react";
+
+function index() {
+  return (
+    <>
+      <Features />
+      <Story />
+      <Homes />
+      <Gallery />
+    </>
+  );
+}
+
+export default index;
